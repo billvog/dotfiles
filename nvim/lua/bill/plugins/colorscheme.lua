8b9072c697c0b1fc38a48accf5377ce04b1712e5
billvog <billvog04@gmail.com>
2024-11-21 19:45:17 +0200

    [nivm] changed theme variant

--- nvim/lua/bill/plugins/colorscheme.lua
+++ nvim/lua/bill/plugins/colorscheme.lua
@@ -3,11 +3,12 @@ return {
   priority = 1000,
   config = function()
     require("rose-pine").setup({
+      variant = "moon",
       styles = {
         transparency = true,
       },
     })
 
-    vim.cmd("colorscheme rose-pine")
+    vim.cmd("colorscheme rose-pine-moon")
   end,
 }
