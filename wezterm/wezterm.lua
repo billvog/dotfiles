6c74e59992d90945ad8ef89f3a752cff201a3740
billvog <billvog04@gmail.com>
2024-11-22 00:05:34 +0200

    [wezterm] decreased bg opacity

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,7 +5,7 @@ local config = wezterm.config_builder()
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
 config.font_size = 18.0
 
-config.window_background_opacity = 0.8
+config.window_background_opacity = 0.9
 config.macos_window_background_blur = 20
 
 config.hide_tab_bar_if_only_one_tab = true
