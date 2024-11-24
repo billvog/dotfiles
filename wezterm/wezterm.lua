a6c217f8aef68a158381d5ff48755d79966673b0
billvog <billvog04@gmail.com>
2024-11-24 19:24:11 +0200

    [wezterm] customize theme

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,8 +5,14 @@ local config = wezterm.config_builder()
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
 config.font_size = 16.0
 
-config.window_background_opacity = 0.9
-config.macos_window_background_blur = 20
+local my_theme = wezterm.color.get_default_colors()
+my_theme.background = "#111111"
+
+config.color_schemes = {
+	["Bill's Theme"] = my_theme,
+}
+
+config.color_scheme = "Bill's Theme"
 
 config.hide_tab_bar_if_only_one_tab = true
 
